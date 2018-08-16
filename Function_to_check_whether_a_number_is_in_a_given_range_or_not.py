4e040d578f1e3ea25f954e7477209ec41f2774d1
Chandan-Pradhan <42320996+Chandan-Pradhan@users.noreply.github.com>
2018-08-17 01:02:25 +0530

    Add files via upload

---
+++ Function_to_check_whether_a_number_is_in_a_given_range_or_not.py
@@ -0,0 +1,7 @@
+def range_check(a,b,c):
+    if b in range(a,c):
+        print('the number is in the range 0-10')
+    else:
+        print('the number is not in range 0-10')
+
+range_check(0,3,11)
